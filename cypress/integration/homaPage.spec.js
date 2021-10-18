852f594ecbe4b5b01e893a1c2eaeabc7d39c3497
Oleh Chepil <chepil742@gmail.com>
2021-10-18 11:30:09 +0300

    second-task-solution

--- cypress/integration/homaPage.spec.js
+++ cypress/integration/homaPage.spec.js
@@ -1,18 +1,10 @@
-describe('general smoke test of main page', () => {
-  it('Main page test', () => {
-    cy.visit('http://automationpractice.com/index.php');
+describe('Home page tests', () => {
+  it('Smoke - Home page (UI, body, header, footer', () => {
+    cy.visit('/index.php');
     cy.url().should('eq', 'http://automationpractice.com/index.php');
-  }); // not shure understand about "content is visible" for home page
-  
-  it('Header test', () => {
-    cy.visit('http://automationpractice.com/index.php');
     cy.get('body')
       .find('header')
       .should('be.visible');
-  });
-
-  it('Footer test', () => {
-    cy.visit('http://automationpractice.com/index.php');
     cy.get('body')
       .find('footer')
       .should('be.visible');
