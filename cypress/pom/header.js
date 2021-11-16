7f1be48ce9a72671f075e28a4db00592762cafc1
Oleh Chepil <chepil742@gmail.com>
2021-11-16 13:32:34 +0200

    code formating

--- cypress/pom/header.js
+++ cypress/pom/header.js
@@ -6,43 +6,43 @@ export class Header extends BasePage {
   }
 
   get contactUsLink() {
-    return this.container.find('#contact-link', {timeout: 10000})
+    return this.container.find('#contact-link', { timeout: 10000 })
   }
 
   get signInLink() {
-    return this.container.find('.header_user_info', {timeout: 10000})
+    return this.container.find('.header_user_info', { timeout: 10000 })
   }
 
   get saleBanner() {
-    return this.container.find('.banner', {timeout: 10000});
+    return this.container.find('.banner', { timeout: 10000 });
   }
 
   get phoneLink() {
-    return this.container.find('.shop-phone', {timeout: 10000});
+    return this.container.find('.shop-phone', { timeout: 10000 });
   }
 
   get headerLogo() {
-    return this.container.find('#header_logo', {timeout: 10000})
+    return this.container.find('#header_logo', { timeout: 10000 })
   }
 
   get searchField() {
-    return this.container.find('#searchbox', {timeout: 10000})
+    return this.container.find('#searchbox', { timeout: 10000 })
   }
 
   get searchButton() {
-    return this.searchField.find('button[name="submit_search"]', {timeout: 10000});
-    }
+    return this.searchField.find('button[name="submit_search"]', { timeout: 10000 });
+  }
 
-  get searchInputField () {
-    return this.searchField.find('#search_query_top', {timeout: 10000});
+  get searchInputField() {
+    return this.searchField.find('#search_query_top', { timeout: 10000 });
   }
 
   get cartElement() {
-    return this.container.find('.shopping_cart', {timeout: 10000})
+    return this.container.find('.shopping_cart', { timeout: 10000 })
   }
 
   get menuSection() {
-    return this.container.find('#block_top_menu', {timeout: 10000})
+    return this.container.find('#block_top_menu', { timeout: 10000 })
   }
 
 
@@ -50,8 +50,8 @@ export class Header extends BasePage {
   clickOnContactUsLink() {
     cy.allure().startStep(`Check header Contact Us Link for open on current tab`)
     this.contactUsLink.not('[target="_blank"]')
-    .should('exist')
-    .should('be.visible');
+      .should('exist')
+      .should('be.visible');
     cy.allure().endStep();
 
     return this
@@ -60,8 +60,8 @@ export class Header extends BasePage {
   clickSignInLink() {
     cy.allure().startStep(`Check header SignIn Link for open on current tab`)
     this.signInLink.not('[target="_blank"]')
-    .should('exist')
-    .should('be.visible');
+      .should('exist')
+      .should('be.visible');
     cy.allure().endStep();
 
     return this
@@ -70,8 +70,8 @@ export class Header extends BasePage {
   checkSaleBanner() {
     cy.allure().startStep(`Check header Sale Banner for visibility`)
     this.saleBanner
-    .should('exist')
-    .should('be.visible');
+      .should('exist')
+      .should('be.visible');
     cy.allure().endStep();
 
     return this
@@ -80,10 +80,10 @@ export class Header extends BasePage {
   checkPhone() {
     cy.allure().startStep(`Check header phone link to contain 'Call us now:' and '0123-456-789'`)
     this.phoneLink
-    .should('exist')
-    .should('be.visible')
-    .contains('Call us now:')
-    .contains('0123-456-789');
+      .should('exist')
+      .should('be.visible')
+      .contains('Call us now:')
+      .contains('0123-456-789');
     cy.allure().endStep();
 
     return this
@@ -92,8 +92,8 @@ export class Header extends BasePage {
   checkHeaderLogo() {
     cy.allure().startStep(`Check header Logo visibility`)
     this.headerLogo
-    .should('exist')
-    .should('be.visible');
+      .should('exist')
+      .should('be.visible');
     cy.allure().endStep();
 
     return this
@@ -102,16 +102,16 @@ export class Header extends BasePage {
   checkSearchField() {
     cy.allure().startStep(`Check header SearchField for placeholder text and Search button`)
     this.searchField
-    .should('exist')
-    .should('be.visible')
+      .should('exist')
+      .should('be.visible')
     this.searchInputField
-    .should('exist')
-    .should('be.visible')
-    .invoke('attr', 'placeholder')
-    .should('contain', 'Search')
+      .should('exist')
+      .should('be.visible')
+      .invoke('attr', 'placeholder')
+      .should('contain', 'Search')
     this.searchButton
-    .should('exist')
-    .should('be.visible');
+      .should('exist')
+      .should('be.visible');
     cy.allure().endStep();
 
     return this
@@ -120,8 +120,8 @@ export class Header extends BasePage {
   checkCart() {
     cy.allure().startStep(`Check header Cart for visibility`)
     this.cartElement
-    .should('exist')
-    .should('be.visible');
+      .should('exist')
+      .should('be.visible');
     cy.allure().endStep();
 
     return this
@@ -130,11 +130,11 @@ export class Header extends BasePage {
   checkMenu() {
     cy.allure().startStep(`Check header Menu to contain: Women, Dresses, T-shirts`)
     this.menuSection
-    .should('exist')
-    .should('be.visible')
-    .should('contain', 'Women')
-    .should('contain', 'Dresses')
-    .should('contain', 'T-shirts')
+      .should('exist')
+      .should('be.visible')
+      .should('contain', 'Women')
+      .should('contain', 'Dresses')
+      .should('contain', 'T-shirts')
     cy.allure().endStep();
 
     return this
